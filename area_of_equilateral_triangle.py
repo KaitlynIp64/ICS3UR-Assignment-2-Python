c1ee2f7857d824baa2010100f6b28208558984ca
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-10-05 21:53:00 +0000

    added code

---
+++ area_of_equilateral_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Kaitlyn Ip
+# Created on: Oct 2022
+# This program calculates the area of an equilateral triangle
+#    with base and height inputted from the user
+
+import math
+
+
+def main():
+    # this function calculates the area of an equilateral triangle
+
+    # input
+    side_of_triangle = int(input("Enter the side of the equilateral triangle (mm): "))
+    # process
+    area_of_triangle = math.sqrt(3) / 4 * (math.pow(side_of_triangle, 2))
+    perimeter_of_triangle = 3 * (side_of_triangle)
+
+    # output
+    print("")
+    print("Area is {0} mm².".format(area_of_triangle))
+    print("Perimeter is {0} mm.".format(perimeter_of_triangle))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
